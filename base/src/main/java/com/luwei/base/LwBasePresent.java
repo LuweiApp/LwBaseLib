b6bf49605796669eb9cb17cc238a5e3cc8d4439d
曾荣基 <cengrongji@talkmoney.cn>
2019-02-13 14:48:15 +0800

    fix:presenter 添加rxjava生命周期管理

--- base/src/main/java/com/luwei/base/LwBasePresent.java
+++ base/src/main/java/com/luwei/base/LwBasePresent.java
@@ -1,10 +1,24 @@
 package com.luwei.base;
 
 
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
 
 public class LwBasePresent<V extends IView> implements IPresent<V> {
 
     private V v;
+    private CompositeDisposable mCompositeDisposable;
+
+    public LwBasePresent(){
+        mCompositeDisposable = new CompositeDisposable();
+    }
+
+    /**
+     * 统一管理RxJava生命周期，防止内存泄露
+     */
+    protected void put(Disposable disposable) {
+        mCompositeDisposable.add(disposable);
+    }
 
     @Override
     public void attachV(V view) {
@@ -14,6 +28,7 @@ public class LwBasePresent<V extends IView> implements IPresent<V> {
     @Override
     public void detachV() {
         v = null;
+        mCompositeDisposable.clear();
     }
 
     protected V getV() {
